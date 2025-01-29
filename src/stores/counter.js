a79efd13bdc4ee4663aead694509c90cfee97e37
waseem abbas <waseemofficee@gmail.com>
2025-01-29 18:48:39 +0500

    use useCounterStore for counter

---
+++ src/stores/counter.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter',{
+state: () =>({
+    count: 0
+    }),
+    actions: {
+        incrementCount(num){
+            this.count += num;
+        },
+        decrementCount(num){
+            this.count -= num;
+        },
+    },
+    getters: {
+    oddOrEven: (state) => {
+        return state.count % 2 === 0 ? 'even' : 'odd';
+    }
+    }
+
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,25 @@
 <template>
 <div>
     <h1>Home</h1>
-    <span class="bg-red-500 text-3xl">{{ count }}</span>
+    <span class="bg-red-500 text-3xl">{{ countStore.count }}</span>
     <div class="d-flex gap-5 align-items-center justify-content-center">
-        <button @click="decrementCount(2)" class="btn btn-primary"> — — </button>
-        <button @click="decrementCount(1)" class="btn btn-primary"> — </button>
-        <button @click="incrementCount(1)" class="btn btn-primary"> <i class="fa-solid fa-plus"></i> </button>
-        <button @click="incrementCount(2)" class="btn btn-primary"> <i class="fa-solid fa-plus"></i> &nbsp; <i class="fa-solid fa-plus"></i> </button>
+        <button @click="countStore.decrementCount(2)" class="btn btn-primary"> — — </button>
+        <button @click="countStore.decrementCount(1)" class="btn btn-primary"> — </button>
+        <button @click="countStore.incrementCount(1)" class="btn btn-primary"> <i class="fa-solid fa-plus"></i> </button>
+        <button @click="countStore.incrementCount(2)" class="btn btn-primary"> <i class="fa-solid fa-plus"></i> &nbsp; <i class="fa-solid fa-plus"></i> </button>
     </div>
     <hr>
     <div class="odd even">
-        <p> Given number is {{ oddOrEven }} </p>
+        <p> Given number is <span class="bg-red-500 text-white">{{ countStore.oddOrEven }}</span> </p>
     </div>
 </div>
 </template>
 
 <script setup>
-import {
-    computed,
-    ref
-} from 'vue';
+import { useCounterStore
+ } from '@/stores/counter';
+const countStore = useCounterStore();
 
-const count = ref(0);
-
-const incrementCount = (num) => {
-    count.value += num;
-}
-const decrementCount = (num) => {
-    count.value -= num;
-}
-const oddOrEven = computed(() => {
-    return count.value % 2 === 0 ? 'even' : 'odd';
-})
 </script>
 
 <style scoped>
