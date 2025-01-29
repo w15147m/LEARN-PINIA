3f31f652f66ddc659d055d72b875c312baf489db
waseem abbas <waseemofficee@gmail.com>
2025-01-29 19:13:17 +0500

    use in child

---
+++ src/components/counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+       <h1>Counter Vue</h1>
+    </div>
+    <p>Count: {{ countStore.count }}</p>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const countStore = useCounterStore();
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,10 +4,14 @@
     <div>
         <button class="btn btn-outline-secondary py text-3xl" @click="countStore.incrementCount(1) ">{{ countStore.count }}</button>
     </div>
+    <div class="pt-2">
+        <counter ></counter>
+    </div>
 </div>
 </template>
 
 <script setup>
+import counter from '@/components/counter.vue';
 import { useCounterStore } from '@/stores/counter';
 
 const countStore = useCounterStore();
